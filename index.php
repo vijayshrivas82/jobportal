36b260e7e6700fef54d6ce891a982a55643c6fd3
vijayshrivas82 <162671199+vijayshrivas82@users.noreply.github.com>
2024-04-27 09:37:15 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Agbalumo&family=Young+Serif&display=swap" rel="stylesheet">
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <!--swiper-->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <title>job portal website</title>
+    <title > login page </title>
+</head>
+<body>
+        <div class="background-video">
+            <video id="bg-Video" autoplay muted loop>
+                <source src="https://cdn.pixabay.com/video/2021/03/08/67358-521707474_tiny.mp4">
+            </video>
+        </div>
+    <!-- Navbar -->
+    <nav class="navbar">
+        <h2 class="navbar-logo"><a href="#">TECHJOB</a></h2>
+        <div class="navbar-menu">
+            <a href="companies.php">Companies</a>
+            <a href="Testimonial.php">Testimonials</a>
+            <a href="about-us.php">About-us</a>
+            <a href="login.php">login</a>
+           
+
+        </div>
+       
+    </nav>
+    <!-- Header -->
+    <header>
+        <h1 class="header-title">
+            FIND YOUR <br> <span>PERFECT JOBS</span><br> EASILY
+        </h1>
+    </header>
+</body>
+</html>
